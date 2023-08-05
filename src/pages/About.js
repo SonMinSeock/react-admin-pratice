d13478d234b87b2d63625b23c3ff9d7548c202d6
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-05 15:10:30 +0900

    mui 이용하여 사이드 네비바 구성

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidenav from "../Sidenav";
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+export default function About() {
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <Sidenav />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <h1>About</h1>
+        </Box>
+      </Box>
+    </>
+  );
+}
---
+++ src/pages/Settings.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidenav from "../Sidenav";
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+export default function Settings() {
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <Sidenav />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <h1>Settings</h1>
+        </Box>
+      </Box>
+    </>
+  );
+}
